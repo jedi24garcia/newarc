582268a04cada073ac27ed51c51a0dd4c9d1d69c
jedi24garcia <jedi24garcia@gmail.com>
2023-02-10 23:58:20 +1300

    new random repo

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+
+password = "admiraL"
+
+while True:
+  user1 = input("Enter password: ")
+  if user1 == password:
+    break
+  else:
+    print("Please try again!")
+
+print("Hello")
