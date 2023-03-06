16519d1dfc182f3bec6f45bb7d02ac01ffdab493
jedi24garcia <jedi24garcia@gmail.com>
2023-03-06 18:04:11 +1300

    failed to add features

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import numpy
 from scipy import misc
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imread
+# from numpy import linalg
 
 img = imread('Tiger.jpg');
+# img_gray = img_array @ [0.2126,  0.7152, 0.0722]
 type(img)
 
 numpy.ndarray
+# img_gray.shape
 
 # plt.imshow(img_gray, cmap="gray")
 plt.imshow(img)
